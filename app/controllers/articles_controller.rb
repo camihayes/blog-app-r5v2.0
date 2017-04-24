2b4ba96e94068cbf09055a057267ba3fb1c74609
Cami Hayes <camihayes72@gmail.com>
2017-04-24 22:11:38 +0000

    implement editing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,16 +10,32 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-    flash[:success] = "Article has been created."
-    redirect_to articles_path
-  else
-    flash.now[:danger] = "Article has not been created"
-    render :new
+      flash[:success] = "Article has been created"
+      redirect_to articles_path
+    else
+      flash.now[:danger] = "Article has not been created"
+      render :new 
     end
   end
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article
+      
+    else
+       flash.now[:danger] = "Article has not been updated"
+       render :edit
+    end
   end
   
   protected
